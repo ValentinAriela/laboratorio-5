d5c803c6a0ca12e6f3da7b70bc82dc85825ac396
Valentina <v.caceres05@ufromail.cl>
2020-12-11 21:56:36 -0300

    ...

---
+++ workspace_ValentinaCaceres/codigo_ascii/codigo_ascii.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+usinf namespace std;
+
+  int ascii(chard letra)
+  
+  {
+   
+   int valorASCII*letra;
+   
+   return valorASCII;
+
+} 
+ int main() {
+
+   coun<<ascii('A')<<end1;
+   coun<<ascii('5')<<end1;
+   coun<<ascii('[')<<end1;
+   coun<<ascii(',')<<end1;
+   return 0;
+
+}
---
+++ workspace_ValentinaCaceres/repaso/repaso.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int numero1;
+  int numero2;
+  int suma;
+
+  printf("Ingrese el primer numero:\n");
+  scanf("%d", &numero1);
+  printf("Ingrese el segundo numero:\n");
+  scanf("%d", &numero2);
+  suma = numero1 + numero2;
+
+  printf("Resultado de la suma es: %d\n" , suma);
+  
+
+
+  return 0;
+}
